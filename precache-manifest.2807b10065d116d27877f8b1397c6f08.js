2ce2ae9e023e4c96a3f96779169f868171f7e4b3
b0rizz <borizkoff@gmail.com>
2019-01-09 21:24:51 +0200

    Updates

--- precache-manifest.2807b10065d116d27877f8b1397c6f08.js
+++ precache-manifest.2807b10065d116d27877f8b1397c6f08.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/reactiven/static/js/runtime~main.a4487751.js"
   },
   {
-    "revision": "61a740dd8539c36e7526",
-    "url": "/reactiven/static/js/main.61a740dd.chunk.js"
+    "revision": "aef190489008f34cab40",
+    "url": "/reactiven/static/js/main.aef19048.chunk.js"
   },
   {
     "revision": "e64baecc21e69f8a5b3c",
     "url": "/reactiven/static/js/1.e64baecc.chunk.js"
   },
   {
-    "revision": "61a740dd8539c36e7526",
-    "url": "/reactiven/static/css/main.820d1986.chunk.css"
+    "revision": "aef190489008f34cab40",
+    "url": "/reactiven/static/css/main.73df74f1.chunk.css"
   },
   {
-    "revision": "c2e74662ab51e77b3ac17e86bee9f6e5",
+    "revision": "c8e34f49028ab5d1197cc2aa8136a341",
     "url": "/reactiven/index.html"
   }
 ];
