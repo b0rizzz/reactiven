b74656e8b451db6c944827822e6234548eaee3b1
b0rizz <borizkoff@gmail.com>
2019-01-09 18:19:02 +0200

    disable auto capitalize

--- src/components/CreateWordForm.js
+++ src/components/CreateWordForm.js
@@ -3,11 +3,11 @@ import Input from './forms/Input';
 
 const CreateWordForm = (props) => (
     <form>
-        <Input type='text' name='en' autocapitalize='false' value={props.word.en} onChange={props.onChange} />
+        <Input type='text' name='en' autocapitalize='off' value={props.word.en} onChange={props.onChange} />
         <br />
-        <Input type='text' name='transcription' autocapitalize='false' value={props.word.transcription} onChange={props.onChange} />
+        <Input type='text' name='transcription' autocapitalize='off' value={props.word.transcription} onChange={props.onChange} />
         <br />
-        <Input type='text' name='bg' autocapitalize='false' value={props.word.bg} onChange={props.onChange} />
+        <Input type='text' name='bg' autocapitalize='off' value={props.word.bg} onChange={props.onChange} />
         <br/>
         <input type='submit' value="Save" onClick={props.onSave} />
     </form>
